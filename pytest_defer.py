be9c321ee119fed6e2c22ea7ce0b44f1ca3bd8b9
Miki Tebeka <miki@353solutions.com>
2021-08-23 17:58:45 +0300

    more info in setup

--- pytest_defer.py
+++ pytest_defer.py
@@ -1,6 +1,10 @@
-import pytest
+"""Provide a "defer" fixture to pytest"""
 
 import logging
+
+import pytest
+
+__version__ = '0.1.0'
 
 
 @pytest.fixture
--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+def find_version():
+    with open('pytest_defer.py') as fp:
+        for line in fp:
+            if '__version__' in line:
+                version = line.split('=')[-1].strip()
+                return version[1:-1]  # trim ''
+
+
+with open('README.md') as fp:
+    long_desc = fp.read()
+
 
 setup(
-    version='0.1.0',
+    version=find_version(),
     name='pytest-defer',
-    py_modules=['pytest_defer'],
+    license='MIT',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    author='Miki Tebeka',
+    author_email='miki@353solutions.com',
+    packges=find_packages(),
     entry_points={
         'pytest11': [
             'defer = pytest_defer',
